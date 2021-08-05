ed214db208dd95a19ea7ae4380322369028ac48f
Craftplacer <craftplacer@craftplacer.moe>
2021-08-05 04:32:44 +0200

    Add application version helper method

---
+++ SimpleClassicTheme.Common/Helpers/HelperMethods.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+
+using System.Reflection;
+
+namespace SimpleClassicTheme.Common.Helpers
+{
+    public class HelperMethods
+    {
+        public static string? GetApplicationVersion()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var versionAttribute = assembly.GetCustomAttribute<AssemblyVersionAttribute>();
+
+            return versionAttribute?.Version;
+        }
+    }
+}
