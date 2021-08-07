6a2e890e76138ae13965ef36dc63519e00e4c868
Craftplacer <craftplacer@craftplacer.moe>
2021-08-07 23:19:04 +0200

    Add paths helper class

---
+++ SimpleClassicTheme.Common/Helpers/Paths.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassicTheme.Common.Helpers
+{
+    public static class Paths
+    {
+        public static string QuickLaunch
+        {
+            get
+            {
+                var appData = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+                return Path.Combine(appData, "Microsoft", "Internet Explorer", "Quick Launch");
+            }
+        }
+    }
+}
